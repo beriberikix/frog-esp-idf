cbdec4127806032dd622ab42d1aeae815a95dd14
Jonathan Beri <jmberi@gmail.com>
2022-10-29 06:10:47 -0700

    scd30 working

--- main/main.c
+++ main/main.c
@@ -12,6 +12,7 @@
 #endif
 
 #include "lc709203f.h"
+#include "scd30.h"
 
 static const char *TAG = "Frog";
 
@@ -43,17 +44,48 @@ static esp_err_t initialize_lc709203f(i2c_dev_t *lc)
     return ESP_OK;
 }
 
-void lc709203f_test(void *pvParameters)
+static esp_err_t initialize_scd30(i2c_dev_t *scd)
+{
+    if (!scd)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    uint16_t version, major_ver, minor_ver;
+    ESP_ERROR_CHECK(scd30_read_firmware_version(scd, &version));
+
+    major_ver = (version >> 8) & 0xf;
+    minor_ver = version & 0xf;
+
+    ESP_LOGI(TAG, "SCD30 Firmware Version: %d.%d", major_ver, minor_ver);
+
+    ESP_LOGI(TAG, "Starting continuous measurement");
+    ESP_ERROR_CHECK(scd30_trigger_continuous_measurement(scd, 0));
+
+    return ESP_OK;
+}
+
+void task(void *pvParameters)
 {
     i2c_dev_t lc;
+    i2c_dev_t scd;
+
+    // lc709203f
     uint16_t voltage = 0, rsoc = 0, ite = 0;
-    float temperature = -274;
+    float bat_temp = -274;
+
+    // scd30
+    float co2, temperature, humidity;
+    bool data_ready;
 
     memset(&lc, 0, sizeof(lc));
 
     ESP_ERROR_CHECK(lc709203f_init_desc(&lc, 0, CONFIG_FROG_I2C_MASTER_SDA, CONFIG_FROG_I2C_MASTER_SCL));
-
     initialize_lc709203f(&lc);
+
+    memset(&scd, 0, sizeof(scd));
+    ESP_ERROR_CHECK(scd30_init_desc(&scd, 0, CONFIG_FROG_I2C_MASTER_SDA, CONFIG_FROG_I2C_MASTER_SCL));
+    initialize_scd30(&scd);
 
     while (1)
     {
@@ -61,12 +93,81 @@ void lc709203f_test(void *pvParameters)
         ESP_ERROR_CHECK(lc709203f_get_rsoc(&lc, &rsoc));
         ESP_ERROR_CHECK(lc709203f_get_cell_ite(&lc, &ite));
         // Temperature in I2C mode. Temperature should be the same as configured.
-        ESP_ERROR_CHECK(lc709203f_get_cell_temperature_celsius(&lc, &temperature));
-        ESP_LOGI("Battery", "Temp: %.1f\tVoltage: %.2f\tRSOC: %d%%\tITE: %.1f%%", temperature, voltage / 1000.0, rsoc,
+        ESP_ERROR_CHECK(lc709203f_get_cell_temperature_celsius(&lc, &bat_temp));
+        ESP_LOGI("Battery", "Temp: %.1f\tVoltage: %.2f\tRSOC: %d%%\tITE: %.1f%%", bat_temp, voltage / 1000.0, rsoc,
             ite / 10.0);
+
+        scd30_get_data_ready_status(&scd, &data_ready);
+        if (data_ready)
+        {
+            esp_err_t res = scd30_read_measurement(&scd, &co2, &temperature, &humidity);
+            if (res != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Error reading results %d (%s)", res, esp_err_to_name(res));
+                // continue;
+            }
+
+            if (co2 == 0)
+            {
+                ESP_LOGW(TAG, "Invalid sample detected, skipping");
+                // continue;
+            }
+
+            ESP_LOGI(TAG, "CO2: %.0f ppm", co2);
+            ESP_LOGI(TAG, "Temperature: %.2f °C", temperature);
+            ESP_LOGI(TAG, "Humidity: %.2f %%", humidity);
+        }
+
         vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
+
+// void scd30_test(void *pvParameters)
+// {
+//     i2c_dev_t dev = {0};
+
+//     ESP_ERROR_CHECK(scd30_init_desc(&dev, 0, CONFIG_FROG_I2C_MASTER_SDA, CONFIG_FROG_I2C_MASTER_SCL));
+
+//     uint16_t version, major_ver, minor_ver;
+//     ESP_ERROR_CHECK(scd30_read_firmware_version(&dev, &version));
+
+//     major_ver = (version >> 8) & 0xf;
+//     minor_ver = version & 0xf;
+
+//     ESP_LOGI(TAG, "SCD30 Firmware Version: %d.%d", major_ver, minor_ver);
+
+//     ESP_LOGI(TAG, "Starting continuous measurement");
+//     ESP_ERROR_CHECK(scd30_trigger_continuous_measurement(&dev, 0));
+
+//     float co2, temperature, humidity;
+//     bool data_ready;
+//     // while (1)
+//     // {
+//     //     vTaskDelay(pdMS_TO_TICKS(2000));
+
+//         scd30_get_data_ready_status(&dev, &data_ready);
+
+//         if (data_ready)
+//         {
+//             esp_err_t res = scd30_read_measurement(&dev, &co2, &temperature, &humidity);
+//             if (res != ESP_OK)
+//             {
+//                 ESP_LOGE(TAG, "Error reading results %d (%s)", res, esp_err_to_name(res));
+//                 // continue;
+//             }
+
+//             if (co2 == 0)
+//             {
+//                 ESP_LOGW(TAG, "Invalid sample detected, skipping");
+//                 // continue;
+//             }
+
+//             ESP_LOGI(TAG, "CO2: %.0f ppm", co2);
+//             ESP_LOGI(TAG, "Temperature: %.2f °C", temperature);
+//             ESP_LOGI(TAG, "Humidity: %.2f %%", humidity);
+//         }
+//     // }
+// }
 
 void app_main(void)
 {
@@ -84,5 +185,6 @@ void app_main(void)
 #endif
 
     ESP_ERROR_CHECK(i2cdev_init());
-    xTaskCreate(lc709203f_test, "lc709203f_test", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    xTaskCreate(task, "sense", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    // xTaskCreate(scd30_test, "scd30_test", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
 }
