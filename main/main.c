b4a0f53bba7e990039f1c48285fd5a28571a4f3a
Jonathan Beri <jmberi@gmail.com>
2022-11-19 21:55:31 +0000

    Add lc709203f to working Golioth example

--- main/main.c
+++ main/main.c
@@ -304,7 +304,48 @@ void app_main(void)
     // Create a background shell/CLI task (type "help" to see a list of supported commands)
     shell_start();
 
+    // If the credentials haven't been set in NVS, we will wait here for the user
+    // to input them via the shell.
+    if (!nvs_credentials_are_set()) {
+        while (1) {
+            golioth_sys_msleep(1000);
+            ESP_LOGW(TAG, "WiFi and golioth credentials are not set");
+            ESP_LOGW(TAG, "Use the shell settings commands to set them, then restart");
+            golioth_sys_msleep(UINT32_MAX);
+        }
+    }
+
+    // Initialize WiFi and wait for it to connect
+    wifi_init(nvs_read_wifi_ssid(), nvs_read_wifi_password());
+    wifi_wait_for_connected();
+
+    // Now we are ready to connect to the Golioth cloud.
+    //
+    // To start, we need to create a client. The function golioth_client_create will
+    // dynamically create a client and return a handle to it.
+    //
+    // The client itself runs in a separate task, so once this function returns,
+    // there will be a new task running in the background.
+    //
+    // As soon as the task starts, it will try to connect to Golioth using the
+    // CoAP protocol over DTLS, with the PSK ID and PSK for authentication.
+    const char* psk_id = nvs_read_golioth_psk_id();
+    const char* psk = nvs_read_golioth_psk();
+
+    // golioth_client_config_t config = {
+    //         .credentials = {
+    //                 .auth_type = GOLIOTH_TLS_AUTH_TYPE_PSK,
+    //                 .psk = {
+    //                         .psk_id = psk_id,
+    //                         .psk_id_len = strlen(psk_id),
+    //                         .psk = psk,
+    //                         .psk_len = strlen(psk),
+    //                 }}};
+    // golioth_client_t client = golioth_client_create(&config);
+    // assert(client);
+
+    // golioth_lightdb_stream_set_int_async(client, "my_stream_int", 15, NULL, NULL);
+
     ESP_ERROR_CHECK(i2cdev_init());
     xTaskCreate(task, "sense", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
-    // xTaskCreate(scd30_test, "scd30_test", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
 }
