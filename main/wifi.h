f25ae075680603dc351012b7cb746cef10982a91
Jonathan Beri <jmberi@gmail.com>
2022-11-20 02:06:06 +0000

    Golioth helper libs

---
+++ main/wifi.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void wifi_init(const char* ssid, const char* password);
+void wifi_wait_for_connected(void);
+bool wifi_wait_for_connected_with_timeout(uint32_t timeout_s);
